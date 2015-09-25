498d2e3d4ed4e6e7dcd0ed3ca570765bb4356c1d
Jon Eisen <jon@metricfeat.com>
2015-09-25 16:19:24 -0500

    Add win checking

--- js/app.js
+++ js/app.js
@@ -50,17 +50,81 @@ function shuffle(array) {
 var shuffledChoices = shuffle(choices)
 
 var squares = document.querySelectorAll('#bingoBoard td')
+  , board = document.querySelector('#bingoBoard')
+  , selectedSquares = makeArray(5, function () { return makeArray(5, function () { return false }) })
+
+selectedSquares[2][2] = 'free'
 
 for(var i=0; i < squares.length; i++) {
+  var row, col
   squares[i].textContent = shuffledChoices[i]
+  squares[i].setAttribute('data-row', row = Math.floor(i / 5))
+  squares[i].setAttribute('data-col', col = i % 5)
+  if (selectedSquares[row][col])
+    squares[i].classList.add('selected')
   squares[i].onclick = onclick
 }
 
 squares[12].textContent = "FREE SQUARE"
 
+// Select
 function onclick(e) {
   var square = e.target
-    , selected = square.classList.contains('selected')
+    , row = parseInt(square.getAttribute('data-row'))
+    , col = parseInt(square.getAttribute('data-col'))
+    , selected = selectedSquares[row][col]
 
+  if (selected === 'free')
+    return console.log('You cant de-select the free square!')
+
+  selectedSquares[row][col] = !selected
   square.classList[selected ? 'remove' : 'add']('selected')
+
+  checkWin(selectedSquares)
+}
+
+// See if we've won given a selectedSquares 2d array
+function checkWin(ss) {
+  var rows = ss
+    , cols = transpose(ss)
+    , diags = [diag(rows), oppdiag(cols)]
+    , checks  = rows.concat(cols).concat(diags)
+    , win = any(checks, function(check) { return all(check, identity) })
+
+  board.classList[win ? 'add' : 'remove']('win')
+}
+
+function identity(x) { return x }
+function any(arr, fn) {
+  for (var i = 0, n = arr.length; i < n; i++) {
+    if (fn(arr[i]))
+      return true
+  }
+  return false
+}
+
+function all(arr, fn) {
+  return !any(arr, function (v) { return !fn(v) })
+}
+
+// matrix transpose
+function transpose(m) {
+  return m.map(function (_,i) {
+    return m.map(function (_,j) {
+      return m[j][i]
+    })
+  })
+}
+
+// matrix diagnol
+function diag(m) {
+  return m.map(function (_,i) { return m[i][i] })
+}
+// matrix opposing diagnol
+function oppdiag(m) {
+  return m.map(function (_,i) { return m[i][4-i] })
+}
+
+function makeArray(n, fn) {
+  return Array.apply(null, {length: n}).map(fn)
 }
